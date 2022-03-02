d0ac44a63d3516d8cf6e1ec17bd15937692ceb47
kazakov.ds <kazakov.ds@phystech.edu>
2022-03-02 12:02:05 +0300

    Scripts

---
+++ 2-1-running-light.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode (GPIO.BCM)
+leds =  [21,20,16,12,7,8,25,24]
+
+for led in leds :
+    GPIO.setup(led,GPIO.OUT)
+
+i=0
+
+while i<3:
+    for led in leds:
+        GPIO.output(led,1)
+        time.sleep(0.2)
+        GPIO.output(led,0)
+    i +=1
+
+GPIO.output(leds,0)
+GPIO.cleanup()
---
+++ 2-3-aux.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode (GPIO.BCM)
+leds =  [21,20,16,12,7,8,25,24]
+aux = [22,23,27,18,15,14,3,2]
+
+GPIO.setup(leds,GPIO.OUT)
+
+GPIO.setup(aux,GPIO.IN)
+
+while True:
+    for i in range(8):
+        aux_value = GPIO.input(aux[i])
+        GPIO.output(leds[i],aux_value)
+
+time.sleep(15)
+
+GPIO.output(leds,0)
+
+GPIO.cleanup()
